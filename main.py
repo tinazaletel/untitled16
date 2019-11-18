71c47f473e3cc2727d3807076f7c5e96b5c52be4
Tina Zaletel <tina.zaletel@mbill.si>
2019-11-18 18:19:56 +0100

    Dodal model Uporabnik

--- main.py
+++ main.py
@@ -2,14 +2,31 @@
 
 # kako polinka[ html datoteko
 
+import random
+import uuid
+from flask import Flask, render_template, request, redirect, make_response
+from modeli import Komentar, db, Uporabnik
 
-from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
+db.create_all()
 
 @app.route("/")
 def prva_stran() :
-    return render_template ("prva_stran.html")
+    sejna_vrednost =request.cookies.get("sejna_vrednost")
+    uporabnik = db.query(Uporabnik).filter_by(sejna_vrednost=sejna_vrednost).first()
+
+    if uporabnik:
+        ime = uporabnik.ime
+
+    else:
+        ime = None
+
+    komentarji = db.query(Komentar) . all()
+
+    return render_template ("prva_stran.html", ime=ime, komentarji=komentarji)
+
 
 #-----------------------------------------------------
 
@@ -44,9 +61,81 @@ def poslji_sporocilo ():
 
 #prva zadeva je povazana s html, druga pa s pythonom
 
+# nova funkcija prijava
+@app.route("/prijava", methods=["POST"])
+def prijava():
+    ime = request.form.get("ime")
+  #  return render_template("prva_stran.html", ime)
+
+    sejna_vrednost = str(uuid.uuid4 ())
+
+    uporabnik = db.query(Uporabnik). filter_by(ime=ime).first()
+    if not uporabnik:
+        uporabnik = Uporabnik(ime=ime, sejna_vrednost=sejna_vrednost)
+    else:
+        uporabnik.sejna_vrednost = sejna_vrednost
+
+    db.add(uporabnik)
+    db.commit()
+    #shranimo v sejno vrednost
+
+    #section token = sejna vrednost
 
 
+  #  ime = request.form.get("ime")
+    odgovor = make_response(redirect("/"))
+    odgovor.set_cookie("sejna_vrednost", sejna_vrednost)
+    return odgovor
+
+#ce napise ("/") bo [lo na tvojo prvo stran
+
+@app.route("/komentar", methods=["POST"])
+def poslji_komentar():
+    vsebina_komentarja = request.form.get("vsebina")
+    # Tukaj se bo shranil komentar v podatkovno bazo
+    sejna_vrednost = request.cookies.get("sejna_vrednost")
+    uporabnik = db.query(Uporabnik).filter_by(sejna_vrednost=sejna_vrednost).first()
+    komentar = Komentar(
+        avtor=uporabnik.ime,
+        vsebina=vsebina_komentarja
+    )
+    db.add(komentar)
+    db.commit()
+
+
+
+
+
+@app.route("/skrito-stevilo")
+def skrito_stevilo():
+    odgovor = make_response(render_template("skrito_stevilo.html"))
+
+    if not request.cookies.get("SkritoStevilo"):
+        stevilo = str(random.randint(1, 20))
+        odgovor.set_cookie("SkritoStevilo", stevilo)
+    return odgovor
+
+
+
+
+@app.route("/poslji-skrito-stevilo", methods=["POST"])
+def poslji_skrito_stevilo():
+    skrito_stevilo = request.cookies.get("SkritoStevilo")
+    vpisano_stevilo =request.form.get("stevilo")
+
+    if skrito_stevilo == vpisano_stevilo:
+        return "PRAVILNO"
+
+    else:
+        return "NI PRAVILNO"
+
+
+
+
+
+
+# return redirect("/") odpre novo stran
 if __name__== '__main__':
-    app.run()
+    app.run(debug=True)
 
 
---
+++ modeli.py
@@ -0,0 +1,14 @@
+from sqla_wrapper import SQLAlchemy
+import os
+db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///podatkovna-baza.sqlite"))
+
+class Komentar(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    avtor = db.Column(db.String)
+    vsebina = db.Column(db.String)
+
+
+class Uporabnik(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ime = db.Column (db.String)
+    sejna_vrednost = db.Column(db.String)
